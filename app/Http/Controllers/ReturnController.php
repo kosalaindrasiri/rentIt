39fccd9c7c6b66897eae2fbc18f26036c2edc708
kosala <kosala@thesanmark.com>
2016-05-02 16:04:25 +0530

    temp

--- app/Http/Controllers/ReturnController.php
+++ app/Http/Controllers/ReturnController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Validator;
+use App\Rent;
+use App\Item;
+use App\Customer;
 
 class ReturnController extends Controller
 {
     public function addWindow() {
         $customers = Customer::all();
-        return view('return.add')->with(compact(['items', 'customers']));    
+        return view('return.add')->with(compact([ 'customers']));    
         
     }
 }
