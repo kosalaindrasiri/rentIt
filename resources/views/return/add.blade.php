9b3689231c1cf799a3f7456902575427eff4daf4
kosala <kosala@thesanmark.com>
2016-05-03 17:13:26 +0530

    Customers Loaded to view

--- resources/views/return/add.blade.php
+++ resources/views/return/add.blade.php
@@ -5,7 +5,17 @@
         <div class="col-sm-12">
 
             <h2>Add Return</h2>
-            
+            <div class="form-group">
+                <label for="selectCustomer" class="col-sm-2 control-label">Customer:</label>
+                <div class="col-sm-4">
+                    <select name="customer_name" class="form-control" id="selectCustomer">
+                        <option disabled selected value>-- Select --</option>
+                        @foreach($customers as $customer)
+                        <option value="{{$customer->id}}">{{$customer->customer_name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
             
         </div>
     </div>
